ea4ad92501df1e43f2b11ed29b5780f8740b667e
SharifDer <sharifderhem@gmail.com>
2026-01-03 19:35:41 +0300

    updating schema to much the new db stucture, basiclly removing unimportant fields

--- schemas/requests.py
+++ schemas/requests.py
@@ -1,8 +1,9 @@
 from pydantic import BaseModel
+from typing import Literal
 
 class CreateBookRequest(BaseModel):
     title: str
     description: str
     age_range: str
-    gender: str
+    gender: Literal["male", "female"]
     price: float
